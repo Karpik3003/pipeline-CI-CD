9f0a5a30087e69e41fc88176a3cd526549a39f35
Dawid Karpczak <karpczak@gmail.com>
2025-04-02 12:35:43 +0200

    Poprawienie ścieżki Dockerfile v2

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "2 + 3 = 5"
 
 if __name__ == "__main__":
-    print(f"2 + 3 = {add(2, 3)}")
+    # Uruchom aplikację na porcie 5000 i pozwól na dostęp z zewnątrz
+    app.run(host="0.0.0.0", port=5000)
